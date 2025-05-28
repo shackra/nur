c6beb833974a2863ec489157777ab7f3054fae07
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-05-27 18:57:20 -0600

    Add package for red4-conflicts

--- packages/red4-conflicts/default.nix
+++ packages/red4-conflicts/default.nix
@@ -9,12 +9,29 @@
 ##########################################################################
 
 {
-  system ? builtins.currentSystem,
-  pkgs ? import <nixpkgs> { inherit system; },
+  rustPlatform,
+  fetchFromGitHub,
+  lib,
 }:
+rustPlatform.buildRustPackage rec {
+  pname = "red4-conflicts";
+  version = "0.4.1";
 
-rec {
-  ngpost = pkgs.libsForQt5.callPackage ./packages/ngpost { };
-  gtkcsslanguageserver = pkgs.callPackage ./packages/gtk-css-language-server { };
-  red4-conflicts = pkgs.callPackage ./packages/red4-conflicts { };
+  src = fetchFromGitHub {
+    owner = "rfuzzo";
+    repo = "Cyberpunk-utility";
+    rev = "master";
+    sha256 = "sha256-LiR+44b9MzWbvkxnDy6cDujgijGmTw0PscdawVS/dh0=";
+    name = src.repo;
+  };
+
+  cargoHash = "sha256-CEXqaP8fif2Y3wTQaOX4XmHkmVSVUAMQG9C76shComs=";
+  sourceRoot = "${src.name}/${pname}";
+
+  meta = with lib; {
+    description = "A conflict-checker app for Cyberpunk 2077 archives";
+    homepage = "https://github.com/rfuzzo/Cyberpunk-utility";
+    license = licenses.unlicense;
+    maintainers = [ maintainers.shackra ];
+  };
 }
